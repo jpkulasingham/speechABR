4519eddb9a33937a33f45afc010bd68f9dc7732d
jpkulasingham <pranjeevank@gmail.com>
2024-03-08 12:03:24 +0100

    Add files via upload

--- pathnames.py
+++ pathnames.py
@@ -1,9 +1,10 @@
 import pathlib
 
-predictor_folder = pathlib.Path(r'E:\LiU\Projects\Interspeech_2023\predictors')
-rawdata_path = pathlib.Path(r'E:\LiU\Projects\SpeechABR\data')
-preprocessed_path = pathlib.Path(r'E:\LiU\Projects\FreefieldABR\SFABR_rev1.0\preprocessed_data')
-clicks_path = pathlib.Path(r'E:\LiU\Projects\FreefieldABR\SFABR_rev1.0\results_clicks\clicks_006')
-speech_path = pathlib.Path(r'E:\LiU\Projects\FreefieldABR\SFABR_rev1.0\results\rev1_006_final_FIR1Hz')
-postanalysis_path = pathlib.Path(r'E:\LiU\Projects\FreefieldABR\SFABR_rev1.0\post_analysis_006')
+predictor_folder = pathlib.Path(r'')
+rawdata_path = pathlib.Path(r'')
+preprocessed_path = pathlib.Path(r'')
+clicks_path = pathlib.Path(r'')
+speech_path = pathlib.Path(r'')
+postanalysis_path = pathlib.Path(r'')
+speech_path_s = pathlib.Path(r'')
 
